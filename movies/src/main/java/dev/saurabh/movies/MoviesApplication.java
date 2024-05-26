3b7cdeaa748bb8ff7240c060a1c7b5d3259272aa
Saurabh Dhaigude <saurabhdhaigude@Saurabhs-MacBook-Pro.local>
2024-05-26 09:49:13 -0600

    components to frontend added

--- movies/src/main/java/dev/saurabh/movies/MoviesApplication.java
+++ movies/src/main/java/dev/saurabh/movies/MoviesApplication.java
@@ -13,8 +13,5 @@ public class MoviesApplication {
 	}
 
 
-
-
-
 }
 
--- movies/src/main/java/dev/saurabh/movies/Review.java
+++ movies/src/main/java/dev/saurabh/movies/Review.java
@@ -18,7 +18,6 @@ public class Review {
     private ObjectId id;
     private String body;
 
-
     public Review(String body) {
         this.body = body;
     }
